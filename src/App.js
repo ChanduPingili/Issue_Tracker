506a851d1c3866e3b8361c46608c22cf2cc8380b
chandupingili <chandrashekarreddypingili@gmail.com>
2024-05-22 17:35:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import EmpNavbar from './components/employee/emp_navbar';
+import EmpIssues from './components/employee/issues';
+function App() {
+  return (
+    <>
+    <EmpNavbar/>
+    <EmpIssues/>
+    </>
+  );
+}
+
+export default App;
